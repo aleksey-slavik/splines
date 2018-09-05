b6132d65f72c330d47023ee6a6a1b282c94f552a
oleksiislavik <oleksii.slavik@sophos.com>
2018-09-05 13:29:06 +0300

    update Solver

--- edu/tomanova/splines/core/Solver.py
+++ edu/tomanova/splines/core/Solver.py
@@ -1,3 +1,5 @@
+import sympy
+from sympy.solvers.solveset import linsolve
 from edu.tomanova.splines.core.SplineBuilder import SplineBuilder
 from edu.tomanova.splines.core.integrate.Integrate import Integrate
 
@@ -18,4 +20,20 @@ class Solver:
             self.splines.append(splineBuilder.spline)
             integral += Integrate(triangle, splineBuilder.spline).integrate()
 
-        return integral
+        system = self.system(integral)
+        params = sympy.symbols("p1:{0}".format(self.rule.count + 1))
+        roots = linsolve(system, params)
+        splines = []
+
+        for spline in self.splines:
+            splines.append(spline.subs(roots))
+
+        self.splines = splines
+
+    def system(self, expression):
+        system = []
+
+        for i in range(self.rule.count):
+            system.append(sympy.diff(expression, sympy.symbols("p{0}".format(i + 1))))
+
+        return system
